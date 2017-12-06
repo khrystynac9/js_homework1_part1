b7db3bf1aa27c22fe73c25f9683f1a69cb686a4d
khrystynac9 <khrystynkakitty@gmail.com>
2017-12-06 20:11:31 +0200

    some fixes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function mydate() {
-    var event = prompt("Please enter the name of the event");
+    var event = prompt("Please enter the name of the event", "disco party");
+    console.log(typeof(event));
     var name = "Emma", place = "Lviv";
     var time = new Date();
     var myDate = time.getHours() + 2;
     var myMinutes = time.getMinutes();
     var a = parseInt(event);
-    if (isNaN(a)) {
+    if (event == "") {
+        alert("you forgot to enter data");
+        mydate();
+    } else  if (isNaN(a)) {
         console.log((name + " has a " + event + " at " + myDate + ":" + myMinutes + " in " + place + " today!!!"));
-    } else {
+    }else{
         alert("please enter a string not a number");
         mydate();
     }
-   
 }
 mydate();
